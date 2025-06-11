998187a1851bcc4fa928bea1e0dd5f8c10e16e87
Orange-Tofu <108827497+Orange-Tofu@users.noreply.github.com>
2025-06-11 10:23:10 +0530

    Basic architecture set

--- Animations/termcolorTEst.py
+++
@@ -1,6 +0,0 @@
-from termcolor import colored
-import time
-
-for i in range(10):
-    print(colored(f"Frame {i}", 'cyan'))
-    time.sleep(0.5)
--- Animations/tqdmTest.py
+++
@@ -1,5 +0,0 @@
-from tqdm import tqdm
-import time
-
-for i in tqdm(range(10), desc="Loading...", ncols=100, colour="green"):
-    time.sleep(0.5)
